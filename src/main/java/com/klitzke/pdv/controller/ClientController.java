7bd87045af2938097e0e168b76f5fbb6029ededf
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-14 00:09:37 -0300

    Implementando um GET no controller de clientes

--- src/main/java/com/klitzke/pdv/controller/ClientController.java
+++ src/main/java/com/klitzke/pdv/controller/ClientController.java
@@ -1,18 +1,28 @@
 package com.klitzke.pdv.controller;
 
+import com.klitzke.pdv.domain.Client;
 import com.klitzke.pdv.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/Clientes/")
-public class clientesControlers {
+@RequestMapping("/clientes/")
+public class ClientController {
 
     //Atributos
     @Autowired
     private ClienteService service;
 
     //GET - Puxar todos os clientes do sistema
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll() {
+        List<Client> clientes = service.findAll();
+        return ResponseEntity.ok().body(clientes);
+    }
 
 }
