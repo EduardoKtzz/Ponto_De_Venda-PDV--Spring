d08b3a62fd5ec2f998cd256e59be513cbe4536ca
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-13 23:32:58 -0300

    Implementando classe de clientes no domain

--- src/main/java/com/klitzke/pdv/domain/Client.java
+++ src/main/java/com/klitzke/pdv/domain/Client.java
@@ -1,16 +1,30 @@
 package com.klitzke.pdv.domain;
 
-@
+import com.klitzke.pdv.enums.Segmento;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_clientes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Client {
 
     //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Include
     private Long id;
-    private int document;
+    private String document;
     private String name;
     private String email;
     private String numero;
 
     //Associações
-    private String segmento;
+    @Enumerated(EnumType.STRING)  //EnumType.‘STRING’ é mais seguro, pois evita problemas quando a ordem do enum mudar.
+    private Segmento segmento;
 
 }
