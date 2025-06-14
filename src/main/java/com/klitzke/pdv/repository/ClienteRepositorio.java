748e8a2fc3acf2a9b53ed317d83cd6eed1c1aa83
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-13 23:43:41 -0300

    Implementando metodos no repositorio de clientes

--- src/main/java/com/klitzke/pdv/repository/ClienteRepositorio.java
+++ src/main/java/com/klitzke/pdv/repository/ClienteRepositorio.java
@@ -4,7 +4,12 @@ import com.klitzke.pdv.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByDocument(String document);
+    Optional<Client> findByEmail(String email);
 
 }
