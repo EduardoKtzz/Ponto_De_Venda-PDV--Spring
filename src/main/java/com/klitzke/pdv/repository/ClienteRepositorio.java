52fd07ef480b05a84e19158378385c9a93ae1de7
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-15 22:26:50 -0300

    Comentando o codigo dos repositorios

--- src/main/java/com/klitzke/pdv/repository/ClienteRepositorio.java
+++ src/main/java/com/klitzke/pdv/repository/ClienteRepositorio.java
@@ -3,9 +3,9 @@ package com.klitzke.pdv.repository;
 import com.klitzke.pdv.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
+//Repositorio para criar os métodos que vão ser usando no Controller de clientes
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Client, Long> {
 
--- src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
+++ src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
@@ -4,6 +4,7 @@ import com.klitzke.pdv.domain.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Repositorio para criar os métodos que vão ser usando no Controller de produtos
 @Repository
 public interface ProdutoRepositorio extends JpaRepository<Produtos, Long> {
 }
