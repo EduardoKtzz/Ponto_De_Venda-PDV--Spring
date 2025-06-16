4681808c5a8804b4ab4487159fe5902399beaec0
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-15 22:18:35 -0300

    Fazendo comentarios no arquivo de config

--- src/main/java/com/klitzke/pdv/config/DataConfig.java
+++ src/main/java/com/klitzke/pdv/config/DataConfig.java
@@ -17,12 +17,12 @@ public class DataConfig {
     @Bean
     public CommandLineRunner carregarDados(ClienteRepositorio clienteRepositorio, ProdutoRepositorio produtoRepositorio) {
         return args -> {
-            // Clientes
+            // Clientes para ser usado de teste, com o seguimentos deles
             clienteRepositorio.save(new Client(null, "12345678900", "João da Silva", "joao@email.com", "27999999999", Segmento.CLIENTE_FINAL));
             clienteRepositorio.save(new Client(null, "98765432100", "Maria Oliveira", "maria@email.com", "27988888888", Segmento.COLABORADOR));
             clienteRepositorio.save(new Client(null, "45678912300", "Carlos Lima", "carlos@email.com", "27987777777", Segmento.PROFISSIONAL));
 
-            // Produtos
+            // Produtos para ser usados como teste
             produtoRepositorio.save(new Produtos(null, "Stick", new BigDecimal("49.90"), 100));
             produtoRepositorio.save(new Produtos(null, "Protetor Solar", new BigDecimal("199.90"), 50));
             produtoRepositorio.save(new Produtos(null, "Sabonete", new BigDecimal("299.00"), 20));
