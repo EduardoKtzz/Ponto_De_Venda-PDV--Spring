875c14bb2c7fc54784ae0fd199c3a068222bc2a2
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-15 22:20:28 -0300

    Fazendo comentarios na classe Controller de clientes

--- src/main/java/com/klitzke/pdv/controller/ClientController.java
+++ src/main/java/com/klitzke/pdv/controller/ClientController.java
@@ -7,18 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
+//Aqui temos o http principal para os clientes, com os seus métodos (GET, POST, DELETE, PUT)
 @RestController
 @RequestMapping("/clientes/")
 public class ClientController {
 
-    //Atributos
+    //Atributos - Injeção de dependência do Spring Boot
     @Autowired
     private ClienteService service;
 
-    //GET - Puxar todos os clientes do sistema
+    //GET - Puxar todos os clientes do sistema por ordem de 'ID'
     @GetMapping
     public ResponseEntity<List<Client>> findAll() {
         List<Client> clientes = service.findAll();
