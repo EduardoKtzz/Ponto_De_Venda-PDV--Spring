fbf8e32c0eb29cbbe27890f0db962615f1e49bc4
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-16 21:11:54 -0300

    Implementando metodo para puxar clientes pelo CPF/CNPJ deles, e adicionando um GET Controller para isso

--- src/main/java/com/klitzke/pdv/controller/ClientController.java
+++ src/main/java/com/klitzke/pdv/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.klitzke.pdv.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -24,5 +25,17 @@ public class ClientController {
         List<Client> clientes = service.findAll();
         return ResponseEntity.ok().body(clientes);
     }
+
+    //GET POR CPF
+    @GetMapping(value = "/document/{document}")
+    public ResponseEntity<Client> findByDocument(@PathVariable String document) {
+        return service.findByDocument(document).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    //GET POR EMAIL
+
+    //POST PARA CRIAR CLIENTES
+
+    //PUT PARA ATUALIZAR CLIENTES
 
 }
--- src/main/java/com/klitzke/pdv/services/ClienteService.java
+++ src/main/java/com/klitzke/pdv/services/ClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -17,6 +18,11 @@ public class ClienteService {
     //Buscar todos os clientes
     public List<Client> findAll() {
         return repositorio.findAll();
+    }
+
+    //Buscar cliente por CPF/CNPJ
+    public Optional<Client> findByDocument(String document) {
+        return repositorio.findByDocument(document);
     }
 
 
