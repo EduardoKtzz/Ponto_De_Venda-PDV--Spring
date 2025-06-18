1154491a00cb6cdb984c1badd8ecb240282a7270
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-17 21:58:50 -0300

    Implementando metodos no repositorio de produtos

--- src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
+++ src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
@@ -4,7 +4,14 @@ import com.klitzke.pdv.domain.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 //Repositorio para criar os métodos que vão ser usando no Controller de produtos
 @Repository
 public interface ProdutoRepositorio extends JpaRepository<Produtos, Long> {
+
+    Optional<Produtos> findByCode(int codigo);
+    Optional<Produtos> findByType(String tipo);
+    Optional<Produtos> findByName(String nome);
+
 }
