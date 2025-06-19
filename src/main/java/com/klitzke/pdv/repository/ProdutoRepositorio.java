0ecdb2e6748a9e6f0e470083b5d33ebdfcd47104
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-18 21:45:16 -0300

    Fazendo ajustes no respositorio de produtos

--- src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
+++ src/main/java/com/klitzke/pdv/repository/ProdutoRepositorio.java
@@ -4,6 +4,7 @@ import com.klitzke.pdv.domain.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 //Repositorio para criar os métodos que vão ser usando no Controller de produtos
@@ -11,7 +12,7 @@ import java.util.Optional;
 public interface ProdutoRepositorio extends JpaRepository<Produtos, Long> {
 
     Optional<Produtos> findByCodigo(int codigo);
-    Optional<Produtos> findByTipo(String tipo);
+    List<Produtos> findByTipo(String tipo);
     Optional<Produtos> findByName(String name);
 
 }
