ed7a155a0d849203ef08f3d4f76242bc6566ac96
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-19 21:46:56 -0300

    Criando a classe de Item pedido e implementando os atributos dela

---
+++ src/main/java/com/klitzke/pdv/domain/ItemPedido.java
@@ -0,0 +1,33 @@
+package com.klitzke.pdv.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    @JsonIgnore
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produtos produtos;
+
+    private int quantidade;
+
+    private BigDecimal precoUnitario;
+}
