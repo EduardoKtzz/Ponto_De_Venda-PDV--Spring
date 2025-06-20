c9f1243622c1a453f9832335be3c1886dad46ef0
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-19 21:47:51 -0300

    Criando a classe de PedidoDTO e implementando os atributos dela e configurações dela

---
+++ src/main/java/com/klitzke/pdv/dto/PedidoDTO.java
@@ -0,0 +1,18 @@
+package com.klitzke.pdv.dto;
+
+import lombok.*;
+import java.util.List;
+
+@Data // @Data já inclui @Getter, @Setter, toString(), equals() e hashCode().
+public class PedidoDTO {
+
+    //Atributos
+    private String clienteDocument;
+    private List<ItemDTO> itens;
+
+    @Data
+    public static class ItemDTO {
+        private int produtoCodigo;
+        private int quantidade;
+    }
+}
