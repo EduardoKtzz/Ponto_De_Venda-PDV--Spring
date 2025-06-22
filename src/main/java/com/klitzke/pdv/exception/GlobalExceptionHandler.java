cc5f1e65d9e4fa51b160b32169628e6e7624b199
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-21 23:50:31 -0300

    Crindo um tratamento de erro para quando o caminho http está errado

--- src/main/java/com/klitzke/pdv/exception/GlobalExceptionHandler.java
+++ src/main/java/com/klitzke/pdv/exception/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.klitzke.pdv.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> httpInvalido(NoResourceFoundException exception, HttpServletRequest request) {
+        String mensagem = exception.getMessage();
+
+        ErrorResponse erro = new ErrorResponse(LocalDateTime.now() ,HttpStatus.BAD_REQUEST.value(), "Caminho inválido", mensagem, request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
 }
