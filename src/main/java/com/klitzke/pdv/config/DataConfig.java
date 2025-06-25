8ebffb51d07732ef43793ebdcb10655a72169c5b
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-25 18:53:30 -0300

    Pequena alteração

--- src/main/java/com/klitzke/pdv/config/DataConfig.java
+++ src/main/java/com/klitzke/pdv/config/DataConfig.java
@@ -3,12 +3,12 @@ package com.klitzke.pdv.config;
 import com.klitzke.pdv.domain.Client;
 import com.klitzke.pdv.domain.Produtos;
 import com.klitzke.pdv.enums.Segmento;
+import com.klitzke.pdv.enums.TipoPessoa;
 import com.klitzke.pdv.repository.ClienteRepositorio;
 import com.klitzke.pdv.repository.ProdutoRepositorio;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.math.BigDecimal;
 
 @Configuration
@@ -18,9 +18,9 @@ public class DataConfig {
     public CommandLineRunner carregarDados(ClienteRepositorio clienteRepositorio, ProdutoRepositorio produtoRepositorio) {
         return args -> {
             // Clientes para ser usado de teste, com o seguimento deles
-            clienteRepositorio.save(new Client(null, "12345678900", "João da Silva", "joao@email.com", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", Segmento.CLIENTE_FINAL));
-            clienteRepositorio.save(new Client(null, "98765432100", "Maria Oliveira", "maria@email.com", "27988888888", 29160357, "SP", "São Paulo", "Copa", "João pedro", 2222, "", Segmento.COLABORADOR));
-            clienteRepositorio.save(new Client(null, "45678912300", "Carlos Lima", "carlos@email.com", "27987777777", 29122223, "RJ", "Rio de Janeiro", "Rocinha", "Miranda", 9, "casa", Segmento.PROFISSIONAL));
+            clienteRepositorio.save(new Client(null, "12345678900", "João da Silva", "joao@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.CLIENTE_FINAL, TipoPessoa.Pessoa_Fisica_PF));
+            clienteRepositorio.save(new Client(null, "12345678922232322", "Pedro Silva", "joo@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Não deseja receber notificações", Segmento.PROFISSIONAL, TipoPessoa.Pessoa_Juridica_PJ));
+            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
 
             // Produtos para ser usados como teste
             produtoRepositorio.save(new Produtos(null, 9888, "HAWA", "Stick", new BigDecimal("49.90"), 100));
