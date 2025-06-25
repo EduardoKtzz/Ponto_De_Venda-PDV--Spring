17e51559aca374ffc1bca1f5f9af551727a366b3
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-25 19:37:28 -0300

    alterando os BEAN do meu sistema, usando para testes

--- src/main/java/com/klitzke/pdv/config/DataConfig.java
+++ src/main/java/com/klitzke/pdv/config/DataConfig.java
@@ -23,11 +23,9 @@ public class DataConfig {
             clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
 
             // Produtos para ser usados como teste
-            produtoRepositorio.save(new Produtos(null, 9888, "HAWA", "Stick", new BigDecimal("49.90"), 100));
-            produtoRepositorio.save(new Produtos(null, 9923, "FERT", "Protetor Solar", new BigDecimal("199.90"), 50));
-            produtoRepositorio.save(new Produtos(null, 7362, "HALB", "Sabonete1", new BigDecimal("299.00"), 20));
-            produtoRepositorio.save(new Produtos(null, 7363, "HALB", "Sabonete2", new BigDecimal("299.00"), 20));
-            produtoRepositorio.save(new Produtos(null, 7364, "HALB", "Sabonete3", new BigDecimal("299.00"), 20));
+            produtoRepositorio.save(new Produtos(null, "9888", "HAWA", "Stick", new BigDecimal("49.90"), 100, "2834728284828"));
+            produtoRepositorio.save(new Produtos(null, "9923", "FERT", "Protetor Solar", new BigDecimal("199.90"), 50, "2834728284828"));
+            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
 
         };
     }
