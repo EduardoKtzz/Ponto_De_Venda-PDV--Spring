7cc05b543940c53726d2c2f48e87753f26c7318d
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-25 19:04:02 -0300

    Implementando a classe de DTO para os produtos, apenas com informações necessarias para cadastro

--- src/main/java/com/klitzke/pdv/dto/ProdutoDTO.java
+++ src/main/java/com/klitzke/pdv/dto/ProdutoDTO.java
@@ -1,4 +1,28 @@
 package com.klitzke.pdv.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutoDTO {
+
+    //Atributos
+    @NotBlank(message = "O codigo do material obrigatório")
+    private String codigo;
+
+    @NotBlank(message = "Digite o tipo de material.")
+    private String tipoMaterial;
+
+    @NotBlank(message = "O nome do material é obrigatório")
+    private String name;
+
+    @NotNull(message = "O preco é obrigatório.")
+    private BigDecimal preco;
+
 }
