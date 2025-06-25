dd57a10d642d8534abe607a1ae1e06bd95812b2b
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-25 19:24:49 -0300

    Classe Mapper para converter o DTO para entidade

---
+++ src/main/java/com/klitzke/pdv/mapper/ProdutoMapper.java
@@ -0,0 +1,29 @@
+package com.klitzke.pdv.mapper;
+
+import com.klitzke.pdv.domain.Produtos;
+import com.klitzke.pdv.dto.ProdutoDTO;
+
+public class ProdutoMapper {
+
+    // Converte de DTO para Entidade
+    public static Produtos toEntity(ProdutoDTO dto) {
+        Produtos produto = new Produtos();
+        produto.setCodigo(dto.getCodigo()); // converte String para int
+        produto.setTipo(dto.getTipoMaterial());
+        produto.setName(dto.getName());
+        produto.setPreco(dto.getPreco());
+        produto.setQuantidadeEstoque(0); // valor inicial padrão
+        return produto;
+    }
+
+    // Converte de Entidade para DTO
+    public static ProdutoDTO toDTO(Produtos produto) {
+        ProdutoDTO dto = new ProdutoDTO();
+        dto.setCodigo(String.valueOf(produto.getCodigo())); // converte int para String
+        dto.setTipoMaterial(produto.getTipo());
+        dto.setName(produto.getName());
+        dto.setPreco(produto.getPreco());
+        return dto;
+    }
+}
+
