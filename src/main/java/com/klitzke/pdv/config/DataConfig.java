f15b92cb8352b3b9646dc87c3498e857a49e72d6
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-07-08 21:28:25 -0300

    Novos dados no data config

--- src/main/java/com/klitzke/pdv/config/DataConfig.java
+++ src/main/java/com/klitzke/pdv/config/DataConfig.java
@@ -21,28 +21,12 @@ public class DataConfig {
             clienteRepositorio.save(new Client(null, "12345678900", "João da Silva", "joao@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.CLIENTE_FINAL, TipoPessoa.Pessoa_Fisica_PF));
             clienteRepositorio.save(new Client(null, "12345678922232322", "Pedro Silva", "joo@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Não deseja receber notificações", Segmento.PROFISSIONAL, TipoPessoa.Pessoa_Juridica_PJ));
             clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-            clienteRepositorio.save(new Client(null, "12345678933", "Bernado da Silva", "joa@email.com", "12/12/2004", "27999999999", 29160123, "ES", "Serra", "Carapina Grande", "Antonio Rangel", 98, "portão azul", null, "Deseja receber notificações", Segmento.COLABORADOR, TipoPessoa.Pessoa_Fisica_PF));
-
 
 
             // Produtos para ser usados como teste
             produtoRepositorio.save(new Produtos(null, "9888", "HAWA", "Stick", new BigDecimal("49.90"), 100, "2834728284828"));
             produtoRepositorio.save(new Produtos(null, "9923", "FERT", "Protetor Solar", new BigDecimal("199.90"), 50, "2834728284828"));
             produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "7362", "HALB", "Sabonete1", new BigDecimal("299.00"), 20, "2834728284828"));
-            produtoRepositorio.save(new Produtos(null, "9888", "HAWA", "Stick", new BigDecimal("49.90"), 100, "2834728284828"));
-
-
-
-
 
         };
     }
